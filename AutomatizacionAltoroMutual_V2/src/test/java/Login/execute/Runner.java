05e670d8ed3f71360c01e13207e9d8d01b3f4ed2
Solgrey <sol.medina@duocuc.cl>
2025-12-03 21:40:44 -0300

    V1.LOGIN beta.1

--- AutomatizacionAltoroMutual_V2/src/test/java/Login/execute/Runner.java
+++ AutomatizacionAltoroMutual_V2/src/test/java/Login/execute/Runner.java
@@ -6,9 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/Login",
+        features = {
+                "src/test/resources/features/Login/AccesoLogin.feature",
+                "src/test/resources/features/Login/IngresoCredencialesCorrectas.feature"
+        },
         glue = {"Login.steps"},
-        plugin = {"pretty"}
+        plugin = {"pretty", "html:target/cucumber-reports.html"}
 )
 public class Runner {
 }
